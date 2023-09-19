ae6952a2a88257a6c0d27fe4b3e534736d2d5c27
Miguel Peixe <miguel.peixe@a8c.com>
2023-09-19 10:08:15 -0300

    feat: add updater

--- newspack-activecampaign.php
+++ newspack-activecampaign.php
@@ -5,7 +5,8 @@
  * Description:     ActiveCampaign integration for Newspack.
  * Author:          Automattic
  * License:         GPL2
- * Version:         1.0.0
+ * Text Domain:     newspack-activecampaign
+ * Version:         0.1.0
  *
  * @package         Newspack
  */
@@ -14,5 +15,17 @@ namespace Newspack\ActiveCampaign;
 
 defined( 'ABSPATH' ) || exit;
 
+// Define NEWSPACK_ACTIVECAMPAIGN_PLUGIN_FILE.
+if ( ! defined( 'NEWSPACK_ACTIVECAMPAIGN_PLUGIN_FILE' ) ) {
+	define( 'NEWSPACK_ACTIVECAMPAIGN_PLUGIN_FILE', __FILE__ );
+}
+
+require_once dirname( __FILE__ ) . '/includes/class-updater.php';
 require_once dirname( __FILE__ ) . '/includes/class-events.php';
 require_once dirname( __FILE__ ) . '/includes/class-campaign-criteria.php';
+
+new Updater(
+	'newspack-activecampaign/newspack-activecampaign.php',
+	NEWSPACK_ACTIVECAMPAIGN_PLUGIN_FILE,
+	'Automattic/newspack-activecampaign'
+);
